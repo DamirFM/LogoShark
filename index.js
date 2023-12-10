53975006299fddc23ac4c15be61d5e2166ead015
DamirFM <yakupovdamir93@gmail.com>
2023-12-10 11:17:01 -0500

    added main ligic of the app

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+// Import inquirer and fs librares
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-
+// Import Circle, Triangle, Square shape-classes
+const Circle = require('./lib/circle');
+const Triangle = require('./lib/triangle');
+const Square = require('./lib/square');
+// Using the inquirer for user input
 inquirer
     .prompt([
         {
@@ -11,8 +15,8 @@ inquirer
         },
         {
             type: 'input',
-            name: 'color',
-            message: 'Add the color, please',
+            name: 'text_color',
+            message: 'Add the color of the text, please',
         },
         {
             type: 'list',
@@ -25,10 +29,27 @@ inquirer
             type: 'input',
             name: 'shape_color',
             message: 'Choose the shapes color, please',
-            choices: [' a hexadecimal number']
         }
     ])
-    .then( (response) => {
-        console.log(response)
+    // .then promise 
+    // callback function waiting for the response from the input and processing it according to our needs
+    .then((response) => {
+        console.log(response);
+        if (response.shape === 'circle') {
+            // if (response.shape === 'circle')
+            // let's create new object circle, based on a class Circle
+            // and take (response.text, response.text_color, response.shape_color) as a parameters
+            // we not taking the shape as a parametre, beacause we will create thee different shapes as an objects  
+            // when we picking the parameters IT IS IMPORTANT that we are pointing promt.array
+            const circle = new Circle (response.text, response.text_color, response.shape_color )
+            // using the fs library and writeFileSync method (which takes two parameters PATH and METHOD) we will create the logo.svg in the example folder
+            fs.writeFileSync('./examples/logo.svg', circle.render());
+        } else if (response.shape === 'square') {
+            const square = new Square (response.text, response.text_color, response.shape_color )
+            fs.writeFileSync('./examples/logo.svg', square.render());
+        } else {
+            const triangle = new Triangle (response.text, response.text_color, response.shape_color )
+            fs.writeFileSync('./examples/logo.svg', triangle.render());
+        }
     })
    
